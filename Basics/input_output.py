0e0d2af0f2b9f9e1df2efd7000490b08d605fb47
kratitechie <krati.techie@gmail.com>
2026-01-09 18:13:52 +0530

    Add basics: variables and input_output

---
+++ Basics/input_output.py
@@ -0,0 +1,6 @@
+name=input("What is your name?\n")
+age=int(input("What is your age?\n"))
+
+age5=age+5
+
+print(f"Hello {name},\n 5 years from now, you will be {age5} years old")
---
+++ Basics/variablees.py
@@ -0,0 +1,28 @@
+name="Krati"
+age=26
+height=5.8
+is_student=True
+
+print('Name:', name)
+print('Age:', age)
+print('Height:', height)
+print('Is Student:', is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+
+print('This block represents simple arithmetic operations:')
+a=4
+b=6
+print('Addition of a+b:\n',a+b)
+print('Subtraction of a-b:\n',a-b)
+print('Multiplication of a*b:\n',a*b)
+print('Division of a/b:\n',a/b)
+print('Modulus of a%b:\n',a%b)
+print('Exponentiation of a**b:\n',a**b)
+print('Floor Division of a//b:\n',a//b)
+print("hello GitHub, this is my basic program to test my skills in Python")
+      
