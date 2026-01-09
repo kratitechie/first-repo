92bffe43b2a84f8b9d6c962f910395d4a2a75558
kratitechie <krati.techie@gmail.com>
2026-01-09 23:21:15 +0530

    Add Loop Basics- For, While and User Controlled Loop

---
+++ Basics/extra while.py
@@ -0,0 +1,5 @@
+command= ""
+while command!="exit":
+    command=input("Give command(Type exit to stop the loop:")
+    print("You entered:", command)
+print ("Loop has exited.")
---
+++ Basics/user_controlled_while.py
@@ -0,0 +1,5 @@
+number=int(input("Enter a positive integer: "))
+while number>0:
+    print (number)
+    number-=1
+print ("Loop ended.")
